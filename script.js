a75c50a19f87f1d016a4a47aef3aa72df3298c23
Naimul07 <naimul.cse.brur@gmail.com>
2023-05-29 10:27:17 +0600

    added dynamically redering elements generate the menu items from a javascript object

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-document.querySelector(".btn").addEventListener("click", ()=>{
+document.querySelector(".btn").addEventListener("click", () => {
     document.querySelector(".navbar").classList.toggle("active");
 });
 
 //Dynamically rendering elements
-//
+//Be able to generate the menu items from a JavaScript object
+const menuItems = [
+    { icon: 'bx bxs-calendar', name: 'Calendar', url: '#' },
+    { icon: 'bx bx-message-rounded-dots', name: 'Added dynamically', url: '#' },
+    { icon: 'bx bx-store', name: 'Project', url: '#' },
+  ];
+  
+  const menuContainer = document.getElementById('menuItems');
+  
+  menuItems.forEach(item => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    const icon = document.createElement('i');
+    const name = document.createElement('span');
+  
+    icon.className = item.icon;
+    name.className = 'link_name';
+    name.textContent = item.name;
+    link.href = item.url;
+  
+    link.appendChild(icon);
+    link.appendChild(name);
+    listItem.appendChild(link);
+    menuContainer.appendChild(listItem);
+  });
+  

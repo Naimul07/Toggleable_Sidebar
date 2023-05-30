baafae228eaa4352a3d9c29cfcb22585d931b3af
Naimul07 <naimul.cse.brur@gmail.com>
2023-05-30 06:41:02 +0600

    all

--- script.js
+++ script.js
@@ -1,122 +1,68 @@
 document.querySelector(".btn").addEventListener("click", () => {
-    document.querySelector(".navbar").classList.toggle("active");
+  document.querySelector(".navbar").classList.toggle("active");
 });
 
 //Dynamically rendering elements
 //Be able to generate the menu items from a JavaScript object
-/* const menuItems = [
-    { icon: 'bx bxs-calendar', name: 'Calendar', url: '#' },
-    { icon: 'bx bx-message-rounded-dots', name: 'Added dynamically', url: '#' },
-    { icon: 'bx bx-store', name: 'Project', url: '#' },
-  ];
-  
-  const menuContainer = document.getElementById('menuItems');
-  
-  menuItems.forEach(item => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    const icon = document.createElement('i');
-    const name = document.createElement('span');
-  
-    icon.className = item.icon;
-    name.className = 'link_name';
-    name.textContent = item.name;
-    link.href = item.url;
-  
-    link.appendChild(icon);
-    link.appendChild(name);
-    listItem.appendChild(link);
-    menuContainer.appendChild(listItem);
-  });
-   */
- /*  const menuItems = [
-    { icon: 'bx bx-home-alt', name: 'Home', url: '#' },
-    { icon: 'bx bxs-calendar', name: 'Calendar', url: '#' },
-    { icon: 'bx bx-message-rounded-dots', name: 'Message', url: '#' },
-    { icon: 'bx bx-store', name: 'Project', url: '#' },
-    { icon: 'bx bxs-cog', name: 'Settings', url: '#' }
-  ];
-  
-  const menuContainer = document.getElementById('menuItems');
-  const currentURL = window.location.href;
-  
-  menuItems.forEach(item => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    const icon = document.createElement('i');
-    const name = document.createElement('span');
-  
-    icon.className = item.icon;
-    name.className = 'link_name';
-    name.textContent = item.name;
-    link.href = item.url;
-    link.classList.toggle('active', item.url === currentURL);
-  
-    link.appendChild(icon);
-    link.appendChild(name);
-    listItem.appendChild(link);
-    menuContainer.appendChild(listItem);
-  
-    link.addEventListener('click', event => {
-      event.preventDefault();
-      const targetURL = event.target.href;
-      navigateTo(targetURL);
-    });
-  });
-  
-  function navigateTo(url) {
-    history.pushState(null, null, url);
-    // Perform additional actions based on the URL navigation
+
+const menuItems = [
+  { icon: 'bx bx-home-alt', name: 'Home', url: 'index.html' },
+  { icon: 'bx bxs-calendar', name: 'Calendar', url: 'calendar.html' },
+  { icon: 'bx bx-message-rounded-dots', name: 'Message', url: 'message.html' },
+  { icon: 'bx bx-store', name: 'Project', url: 'project.html' },
+  { icon: 'bx bxs-cog', name: 'Settings', url: 'settings.html' }
+];
+
+const menuContainer = document.getElementById('menuItems');
+const currentURL = window.location.href;
+
+menuItems.forEach(item => {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  const icon = document.createElement('i');
+  const name = document.createElement('span');
+
+  icon.className = item.icon;
+  name.className = 'link_name';
+  name.textContent = item.name;
+  link.href = item.url;
+  // link.classList.toggle('active', item.url === currentURL || item.url === currentURL.split('#')[0]);
+
+  link.appendChild(icon);
+  link.appendChild(name);
+  listItem.appendChild(link);
+  menuContainer.appendChild(listItem);
+
+});
+
+
+class HistoryManager {
+  back() {
+    window.history.back();
   }
-   */
-  const menuItems = [
-    { icon: 'bx bx-home-alt', name: 'Home', url: '#' },
-    { icon: 'bx bxs-calendar', name: 'Calendar', url: '#calendar' },
-    { icon: 'bx bx-message-rounded-dots', name: 'Message', url: '#message' },
-    { icon: 'bx bx-store', name: 'Project', url: '#project' },
-    { icon: 'bx bxs-cog', name: 'Settings', url: '#settings' }
-  ];
-  
-  const menuContainer = document.getElementById('menuItems');
-  const currentURL = window.location.href;
-  
-  menuItems.forEach(item => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    const icon = document.createElement('i');
-    const name = document.createElement('span');
-  
-    icon.className = item.icon;
-    name.className = 'link_name';
-    name.textContent = item.name;
-    link.href = item.url;
-    link.classList.toggle('active', item.url === currentURL || item.url === currentURL.split('#')[0]);
-  
-    link.appendChild(icon);
-    link.appendChild(name);
-    listItem.appendChild(link);
-    menuContainer.appendChild(listItem);
-  
-    link.addEventListener('click', event => {
-      event.preventDefault();
-      const targetURL = event.target.href;
-      navigateTo(targetURL);
-    });
-  });
-  
-  function navigateTo(url) {
-    history.pushState(null, null, url);
-    handleMenuSelection(url);
+/* 
+  backTo(n) {
+    window.history.go(n);
+  } */
+
+  forward() {
+    window.history.forward();
   }
-  
-  function handleMenuSelection(url) {
-    const links = document.querySelectorAll('.nav-links a');
-    links.forEach(link => {
-      link.classList.toggle('active', link.href === url || link.href === url.split('#')[0]);
-    });
+
+  getHistory() {
+    alert(window.history.length);
   }
-  
-  window.addEventListener('popstate', () => {
-    handleMenuSelection(window.location.href);
-  });
-  
+}
+
+// Usage
+const historyManager = new HistoryManager();
+function next() {
+  historyManager.forward();    // Call the forward() method
+
+}
+function backward() {
+  historyManager.back();       // Call the back() method
+ 
+}
+
+
